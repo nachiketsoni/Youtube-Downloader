877cda3554668985531caee510d507d4e4bc25b4
nachiketsoni <nxchikxt@gmail.com>
2023-01-25 16:15:42 +0530

    Optimization

--- Youtube.py
+++ Youtube.py
@@ -1,16 +1,14 @@
 from pytube import YouTube
 
-def YoutubeDownloader(link ,check):
-    def Download(require):
+def YoutubeDownloader(link ,Type):
         print("Please Wait...")
         youtube_Output = YouTube(link)
-        if require == 1:
+        if Type == "1":
             result = youtube_Output.streams.filter(progressive = True)
-        elif require == 2:
+        elif Type == "2":
             result = youtube_Output.streams.filter(only_audio=True)
         else:
             print("Error Wrong Input!!")
-
         resul = list(enumerate(result))
         for i in resul:
             print(f"Index : {i[0]} | {i[1]}")
@@ -19,11 +17,8 @@ def YoutubeDownloader(link ,check):
         result[strm].download()
         print("successfully downloaded")
 
-    if check =="1":
-        Download(1)
-    elif check =="2":
-        Download(2)
+
     
 link=input("Enter The Youtube Link: ")
-check = input("Press 1. Dowload as Video \nPress 2. Download as Audio \n")
-YoutubeDownloader(link,check)
+Type = input("Press 1. Dowload as Video \nPress 2. Download as Audio \n")
+YoutubeDownloader(link,Type)
